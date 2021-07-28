1845919fb04fddfd1df463c771504571c22bf3dc
Aviral3888 <63301012+Aviral3888@users.noreply.github.com>
2021-07-28 19:52:28 +0530

    Footer component

--- src/components/Footer/Footer.component.jsx
+++ src/components/Footer/Footer.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { AiFillShop } from 'react-icons/ai';
 import { RiCustomerService2Fill } from 'react-icons/ri';
+import { GiTicket } from 'react-icons/gi';
+import { BsNewspaper } from 'react-icons/bs';
 
 
 const Footer = () => {
@@ -30,50 +32,81 @@ const Footer = () => {
                         </div>
                     </div>
 
-                    {/* Second Part */}
-                    <div className="flex justify-evenly">
+                </div>
+            </div>
 
-                        <div className="flex flex-col gap-4 items-center text-gray-400">
-                            <div>
-                                <RiCustomerService2Fill
-                                    className="w-12 h-12 font-light"
-                                />
-                            </div>
-                            <h2 className="text-xs font-medium">
-                                24/7 CUSTOMER CARE
-                            </h2>
+            {/* Second Part */}
+            <div className="bg-footer-700 py-4">
+                <div className="flex justify-evenly">
+
+                    <div className="flex flex-col gap-4 items-center text-gray-300 hover:text-white cursor-pointer">
+                        <div>
+                            <RiCustomerService2Fill
+                                className="w-12 h-12 font-light"
+                            />
                         </div>
-
-                        <div className="flex flex-col gap-4 items-center text-gray-400">
-                            <div>
-                                <RiCustomerService2Fill
-                                    className="w-12 h-12 font-light"
-                                />
-                            </div>
-                            <h2 className="text-xs font-medium">
-                                24/7 CUSTOMER CARE
-                            </h2>
-                        </div>
-
-                        <div className="flex flex-col gap-4 items-center text-gray-400">
-                            <div>
-                                <RiCustomerService2Fill
-                                    className="w-12 h-12 font-light"
-                                />
-                            </div>
-                            <h2 className="text-xs font-medium">
-                                24/7 CUSTOMER CARE
-                            </h2>
-                        </div>
-
-
-
-                        {/* <div></div>
-                        <div></div> */}
+                        <h2 className="text-xs">
+                            24/7 CUSTOMER CARE
+                        </h2>
                     </div>
 
+                    <div className="flex flex-col gap-4 items-center text-gray-300 hover:text-white cursor-pointer">
+                        <div>
+                            <GiTicket
+                                className="w-12 h-12 font-light"
+                            />
+                        </div>
+                        <h2 className="text-xs ">
+                            BOOKING CONFIRMATION
+                        </h2>
+                    </div>
 
-                    <div></div>
+                    <div className="flex flex-col gap-4 items-center text-gray-300 hover:text-white cursor-pointer">
+                        <div>
+                            <BsNewspaper
+                                className="w-12 h-12 font-light"
+                            />
+                        </div>
+                        <h2 className="text-xs">
+                            SUBSCRIBE TO NEWSLETTER
+                        </h2>
+                    </div>
+
+                </div>
+            </div>
+
+            {/* Third Part */}
+            <div className="bg-footer-800 w-full py-10">
+                <div className="container mx-auto px-lg text-gray-400 flex flex-col gap-6">
+
+                    <div className="flex flex-col gap-4 text-xs ">
+                        <h2 className="font-medium">
+                            MOVIES PLAYING IN NATIONAL CAPITAL REGION (NCR)
+                        </h2>
+                        <p>
+                            Roohi | Mast Kalandar | Mumbai Saga
+                        </p>
+                    </div>
+
+                    <div className="flex gap-4 items-center">
+                        <div className="w-full h-0.5 bg-gray-500">
+                        </div>
+                        <div className="w-24 h-16">
+                            <img
+                                src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png"
+                                alt="Logo"
+                                className="w-full h-full" />
+                        </div>
+                        <div className="w-full h-0.5 bg-gray-500">
+                        </div>
+                    </div>
+
+                    <div className="text-xs text-center text-gray-500">
+                        <p>Copyright </p>
+                        <p>
+                            The content and images used on this site are copyright protected and copyrights vests with the respective owners. The usage of the content and images on this website is intended to promote the works and no endorsement of the artist shall be implied. Unauthorized use is prohibited and punishable by law.
+                        </p>
+                    </div>
                 </div>
             </div>
         </>
